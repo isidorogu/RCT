7f736b4f3bab4537799bbe78e2cd9418fc89b8f9
isidorogu <isidoro.gu@gmail.com>
2020-03-29 17:58:15 -0600

    balance_table corregido

--- R/balance_table.R
+++ R/balance_table.R
@@ -17,49 +17,50 @@
 #' @importFrom magrittr %>%
 balance_table<-function(data, treatment) {
   
-  variables<-NULL
-  data <- data %>% dplyr::arrange(!!rlang::sym(treatment))
-  
-  valores_trat <- base::unique(dplyr::pull(data, !!rlang::sym(treatment)))
-  
-  trats<-valores_trat[2:base::length(valores_trat)]
-  
-  bal_tables<-purrr::map(trats, function (x)
-          dplyr::filter(data, !!rlang::sym(treatment) == valores_trat[1] | !!rlang::sym(treatment) ==  !!x))
-  
-  bal_tables<-purrr::map(bal_tables, function (x) 
-                    tidyr::pivot_longer(x, names_to = "variables", values_to = "value", -treatment) )
-  
-  
-  # Creo por separado la primera para poner la media de control 
-
-  bal_tables<-purrr::map(bal_tables, function(x) x %>% 
-                    dplyr::group_by(variables) %>% 
-                    dplyr::summarise(Media_control = stats::t.test(value~treatment)$estimate[1],
-                              Media_trat = stats::t.test(value~treatment)$estimate[2], 
-                              p_value = stats::t.test(value~treatment)$p.value))
-  
-  valores_trat<-base::as.character(valores_trat[2:base::length(valores_trat)])
-  
-  bal_tables <- purrr::map2_dfc(.x = bal_tables, .y = valores_trat,
-                         function(x,y) dplyr::rename_all(x, ~stringr::str_c(., y)))
-  
-  #Quedandome solo con una de variables y media_control
-  medias_control <- base::names(bal_tables %>% dplyr::select(dplyr::contains("control")) )
-  variables_nombres <- base::names(bal_tables %>% dplyr::select(dplyr::contains("variables")))
-  medias_trat <- base::names(bal_tables %>% dplyr::select(dplyr::contains("trat")))
-  p_values <-base::names(bal_tables %>% dplyr::select(dplyr::contains("p_value")))
-  
-  bal_tables<-
-    bal_tables %>% 
-    dplyr::select(tidyselect::all_of(variables_nombres[1]), 
-                  tidyselect::all_of(medias_control[1]), 
-                  tidyselect::all_of(medias_trat), 
-                  tidyselect::all_of(p_values))
+    variables<-NULL 
     
-  
-  
-  
+    data <- data %>% dplyr::arrange(!!rlang::sym(treatment))
+    
+    valores_trat <- base::unique(dplyr::pull(data, !!rlang::sym(treatment)))
+    
+    trats<-valores_trat[2:base::length(valores_trat)]
+    
+    bal_tables<-purrr::map(trats, function (x)
+      data %>%
+        dplyr::filter(!!rlang::sym(treatment) == valores_trat[1] | !!rlang::sym(treatment) ==  !!x))
+    
+    
+    
+    bal_tables<-purrr::map(bal_tables, function (x) x %>%
+                             tidyr::pivot_longer(names_to = "variables", values_to = "value", -treatment) )
+    
+    
+    # Creo por separado la primera para poner la media de control 
+    
+    bal_tables<-purrr::map(bal_tables, function(x) x %>% 
+                             dplyr::group_by(variables) %>% 
+                             dplyr::summarise(Media_control = stats::t.test(value~!!rlang::sym(treatment))$estimate[1],
+                                              Media_trat = stats::t.test(value~!!rlang::sym(treatment))$estimate[2], 
+                                              p_value = stats::t.test(value~!!rlang::sym(treatment))$p.value))
+    
+    valores_trat<-base::as.character(valores_trat[2:base::length(valores_trat)])
+    
+    bal_tables <- purrr::map2_dfc(.x = bal_tables, .y = valores_trat,
+                                  function(x,y) dplyr::rename_all(x, ~stringr::str_c(., y)))
+    
+    #Quedandome solo con una de variables y media_control
+    medias_control <- base::names(bal_tables %>% dplyr::select(dplyr::contains("control")) )
+    variables_nombres <- base::names(bal_tables %>% dplyr::select(dplyr::contains("variables")))
+    medias_trat <- base::names(bal_tables %>% dplyr::select(dplyr::contains("trat")))
+    p_values <-base::names(bal_tables %>% dplyr::select(dplyr::contains("p_value")))
+    
+    bal_tables<-
+      bal_tables %>% 
+      dplyr::select(tidyselect::all_of(variables_nombres[1]), 
+                    tidyselect::all_of(medias_control[1]), 
+                    tidyselect::all_of(medias_trat), 
+                    tidyselect::all_of(p_values))
+    
 }
 
 
